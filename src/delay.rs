aeca7abbd0cacf3378144aea45fee398208ec0e1
Adam Gausmann <adam@gaussian.dev>
2024-03-22 22:06:53 -0500

    Update to embedded hal 1.0

--- src/delay.rs
+++ src/delay.rs
@@ -16,7 +16,17 @@ impl CycleDelay {
     }
 }
 
-impl embedded_hal_1::delay::DelayUs for CycleDelay {
+impl embedded_hal_1::delay::DelayNs for CycleDelay {
+    fn delay_ns(&mut self, ns: u32) {
+        // Widen to u64 to ensure no overflow
+        // The QingKe RISC-V2A executes an addi in 2 cycles
+        let cycles = ns as u64 * self.rate.to_Hz() as u64 / 2_000_000_000;
+
+        unsafe {
+            qingke::riscv::asm::delay(cycles as u32);
+        }
+    }
+
     fn delay_us(&mut self, us: u32) {
         // Widen to u64 to ensure no overflow
         // The QingKe RISC-V2A executes an addi in 2 cycles
@@ -30,18 +40,18 @@ impl embedded_hal_1::delay::DelayUs for CycleDelay {
 
 impl embedded_hal_02::blocking::delay::DelayUs<u32> for CycleDelay {
     fn delay_us(&mut self, us: u32) {
-        embedded_hal_1::delay::DelayUs::delay_us(self, us as _);
+        embedded_hal_1::delay::DelayNs::delay_us(self, us as _);
     }
 }
 
 impl embedded_hal_02::blocking::delay::DelayUs<u16> for CycleDelay {
     fn delay_us(&mut self, us: u16) {
-        embedded_hal_1::delay::DelayUs::delay_us(self, us as _);
+        embedded_hal_1::delay::DelayNs::delay_us(self, us as _);
     }
 }
 
 impl embedded_hal_02::blocking::delay::DelayUs<u8> for CycleDelay {
     fn delay_us(&mut self, us: u8) {
-        embedded_hal_1::delay::DelayUs::delay_us(self, us as _);
+        embedded_hal_1::delay::DelayNs::delay_us(self, us as _);
     }
 }
